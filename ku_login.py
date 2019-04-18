ecab40a35378ad49186bd56f5e4d0c7452f1fed1
lastthanya <iamjui2010@hotmail.com>
2019-04-18 13:38:46 +0700

    login

---
+++ ku_login.py
@@ -0,0 +1,31 @@
+import requests
+import getpass
+def ku_login():
+    with requests.Session() as session:
+        username = input("username : ")
+        password = getpass.getpass('password: ')
+        payload_login = {
+            "form_username" : username,
+            "form_password": password,
+            "zone":"0"
+        }
+        headers = {
+            "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding" : "gzip, deflate, br",
+            "Accept-Language" : "en-US,en;q=0.8,th;q=0.6",
+            "Cache-Control" : "max-age=0",
+            "Connection" : "keep-alive",
+            "Content-Length" : "58",
+            "Content-Type" : "application/x-www-form-urlencoded",
+            "Host" : "stdregis.ku.ac.th",
+            "Origin" : "https://stdregis.ku.ac.th",
+            "Referer" : "https://stdregis.ku.ac.th/_Login.php",
+            "Upgrade-Insecure-Requests" : "1",
+            "User-Agent" : "Mozilla/4.5 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) "
+        }
+        login = session.post("https://stdregis.ku.ac.th/_Login.php", data=payload_login ,headers=headers)
+        checkLogin = "_Member_Information.php" in login.text
+        if ( checkLogin ):
+            return True
+        else :
+            return False
